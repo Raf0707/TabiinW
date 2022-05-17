4cf3d61330c4a766c9f83c25aefdcb94684c9c62
DanilaDevx21x <rswayfarersx@gmail.com>
2022-05-17 17:37:01 +0600

    Generate clean project

---
+++ app/src/main/java/com/example/tabiinw/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tabiinw;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
