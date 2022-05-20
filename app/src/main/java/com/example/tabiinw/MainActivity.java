2c8c332db6eafc03eba97d66b8cd74e4225777a0
DanilaDevx21x <rswayfarersx@gmail.com>
2022-05-20 22:44:17 +0600

    Maked Counter module-directory and created class CounterRecord

--- app/src/main/java/com/example/tabiinw/MainActivity.java
+++ app/src/main/java/com/example/tabiinw/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.tabiinw;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.tabiinw.koran.KoranFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, new KoranFragment()).commit();
     }
 }
--- app/src/main/java/com/example/tabiinw/SuraItem.java
+++
@@ -1,2 +0,0 @@
-package com.example.tabiinw;public class SuraItem {
-}
